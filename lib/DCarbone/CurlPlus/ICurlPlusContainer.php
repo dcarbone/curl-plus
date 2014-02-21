38493f3d0d9d84dd6e4b17ae7e7b40bb0219925a
Daniel Carbone <daniel.p.carbone@gmail.com>
2014-02-21 12:08:50 -0600

    - Renaming IOCurlContainer to ICurlPlusContainer to keep up with the new naming convention. - Interface methods now must return the class which implements the interface

---
+++ lib/DCarbone/CurlPlus/ICurlPlusContainer.php
@@ -0,0 +1,59 @@
+<?php namespace DCarbone\CurlPlus;
+
+/**
+ * Interface ICurlPlusContainer
+ * @package DCarbone\CurlPlus
+ */
+interface ICurlPlusContainer
+{
+    /**
+     * @return CurlPlusClient
+     */
+    public function &getClient();
+
+    /**
+     * @return array
+     */
+    public function getRequestHeaders();
+
+    /**
+     * @param array $headers
+     * @return ICurlPlusContainer
+     */
+    public function setRequestHeaders(array $headers);
+
+    /**
+     * @param string $header
+     * @return ICurlPlusContainer
+     */
+    public function addRequestHeaderString($header);
+
+    /**
+     * @param int $opt
+     * @param mixed $value
+     * @return ICurlPlusContainer
+     */
+    public function setCurlOpt($opt, $value);
+
+    /**
+     * @param int $opt
+     * @return ICurlPlusContainer
+     */
+    public function removeCurlOpt($opt);
+
+    /**
+     * @param array $opts
+     * @return ICurlPlusContainer
+     */
+    public function setCurlOpts(array $opts);
+
+    /**
+     * @return array
+     */
+    public function getCurlOpts();
+
+    /**
+     * @return ICurlPlusContainer
+     */
+    public function resetCurlOpts();
+}
--- lib/DCarbone/CurlPlus/IOCurlContainer.php
+++ lib/DCarbone/CurlPlus/IOCurlContainer.php
@@ -2,58 +2,13 @@
 
 /**
  * Interface IOCurlContainer
+ *
+ * This interface will be removed soon.
+ *
+ * @deprecated
  * @package DCarbone\CurlPlus
  */
-interface IOCurlContainer
+interface IOCurlContainer extends ICurlPlusContainer
 {
-    /**
-     * @return CurlPlusClient
-     */
-    public function &getClient();
 
-    /**
-     * @return array
-     */
-    public function getRequestHeaders();
-
-    /**
-     * @param array $headers
-     * @return void
-     */
-    public function setRequestHeaders(array $headers);
-
-    /**
-     * @param string $header
-     * @return void
-     */
-    public function addRequestHeaderString($header);
-
-    /**
-     * @param int $opt
-     * @param mixed $value
-     * @return void
-     */
-    public function setCurlOpt($opt, $value);
-
-    /**
-     * @param int $opt
-     * @return void
-     */
-    public function removeCurlOpt($opt);
-
-    /**
-     * @param array $opts
-     * @return void
-     */
-    public function setCurlOpts(array $opts);
-
-    /**
-     * @return array
-     */
-    public function getCurlOpts();
-
-    /**
-     * @return void
-     */
-    public function resetCurlOpts();
 }
